b85894db344b4347ad34da0fc7742c36cdf618e9
connell-denvertechservices <d.connell@colostate.edu>
2026-09-09 22:22:46 +0000

    Created a Python notebook, Notebook.ipynb for analyzing the database

--- Class 5.sql
+++ Class 5.sql
@@ -4,5 +4,11 @@
 use database db_module1;
 use schema academic;
 
+select class_id,
+    class_code,
+    class_name,
+    syllabus:textbook::string as textbook
+from classes
+where textbook is null;
 -- SQL Challenge (Activity)
 -- Which classes do not have a specified textbook?
